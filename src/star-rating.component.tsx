7da12229513dce603851e0f5ba1795af75d27b54
liem nguyen <elminguyen@gmail.com>
2025-04-19 00:50:05 +0300

    add basic logic

--- src/star-rating.component.tsx
+++ src/star-rating.component.tsx
@@ -1,9 +1,27 @@
+import { useState } from "react";
+import './star-rating.styles.css';
+
 const StarRating = () => {
+  const maxRating = 5;
+  const [ currentRating, setCurrentRating ] = useState(0);
+  const onClickHandling = (ratingValue: number) => {
+    setCurrentRating(ratingValue);
+  }
+
   return (
-    <div>
-      // stars
-    </div>
+    <>
+      <div className="star-rating-container">
+        {/* stars */}
+        <p>Rating: {currentRating}</p>
+        {
+          [...Array(maxRating)].map((_, index) => {
+            const ratingValue = index + 1;
+            return <p key={index} onClick={() => onClickHandling(ratingValue)} >{ratingValue}</p>
+        })
+        }
+      </div>
+    </>
   );
 };
 
-export { StarRating };
+export default StarRating;
